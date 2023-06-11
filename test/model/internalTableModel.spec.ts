309ab132a144ae55e05feb56b299df5b027280f7
Lucas Vossen <lv_code@outlook.com>
2023-06-11 20:36:36 +0200

    add test file

---
+++ test/model/internalTableModel.spec.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-new */
+import { assert, expect } from 'chai';
+import { InternalTableModel } from '../../src/model/InternalTableModel';
+
+
+describe('InternalTableModel', () => {
+
+
+   it('instantiates an empty table of specified size for valid size arguments', () => {
+      const rowTotal = 4;
+      const columnTotal = 3;
+      const table = [
+         [null, null, null, null],
+         [null, null, null, null],
+         [null, null, null, null]
+      ];
+      const obj = new InternalTableModel(rowTotal, columnTotal);
+      assert.equal(obj.rowTotal, rowTotal);
+      assert.equal(obj.columnTotal, columnTotal);
+      assert.notEqual(obj.rowTotal, 2);
+      assert.notEqual(obj.columnTotal, 2);
+      assert.deepEqual(obj.getTableClone(), table);
+   });
+
+
+   it('throws a range error for invalid size arguments', () => {
+      expect(() => new InternalTableModel(0, 0)).to.throw(RangeError, 'Arguments rowTotal and columnTotal must be greater than zero');
+   });
+
+
+});
