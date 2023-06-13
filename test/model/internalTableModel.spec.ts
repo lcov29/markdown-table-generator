bd8f3baa0fdb0b2ed9528f0f6e1b1dcc95ed2fa7
Lucas Vossen <lv_code@outlook.com>
2023-06-13 18:04:51 +0200

    add test for method addRowAt()

--- test/model/internalTableModel.spec.ts
+++ test/model/internalTableModel.spec.ts
@@ -4,8 +4,7 @@ import { InternalTableModel } from '../../src/model/InternalTableModel';
 import { TitleContent } from '../../src/model/types';
 
 
-describe('InternalTableModel', () => {
-
+describe('InternalTableModel constructor', () => {
 
    it('instantiates an empty table of specified size for valid size arguments', () => {
       const rowTotal = 4;
@@ -28,5 +27,54 @@ describe('InternalTableModel', () => {
       expect(() => new InternalTableModel(0, 0)).to.throw(RangeError, 'Arguments rowTotal and columnTotal must be greater than zero');
    });
 
+});
+
+
+
+describe('InternalTableModel.addRowAt()', () => {
+
+   it('add new row between existing rows', () => {
+      const obj = new InternalTableModel(3, 2);
+      obj.addRowAt(1);
+      const table: (TitleContent | null)[][] = [
+         [{ type: 'title', title: '', columnAlignment: 'left' }, null, null, null],
+         [{ type: 'title', title: '', columnAlignment: 'left' }, null, null, null]
+      ];
+      assert.deepEqual(obj.getTableClone(), table);
+   });
+
+
+   it('add new row at end of existing rows with index matching array length', () => {
+      const obj = new InternalTableModel(4, 3);
+      obj.addRowAt(3);
+      const table: (TitleContent | null)[][] = [
+         [{ type: 'title', title: '', columnAlignment: 'left' }, null, null, null, null],
+         [{ type: 'title', title: '', columnAlignment: 'left' }, null, null, null, null],
+         [{ type: 'title', title: '', columnAlignment: 'left' }, null, null, null, null]
+      ];
+      assert.deepEqual(obj.getTableClone(), table);
+   });
+
+
+   it('add new row at end of existing rows with index exceeding array length', () => {
+      const obj = new InternalTableModel(4, 3);
+      obj.addRowAt(102);
+      const table: (TitleContent | null)[][] = [
+         [{ type: 'title', title: '', columnAlignment: 'left' }, null, null, null, null],
+         [{ type: 'title', title: '', columnAlignment: 'left' }, null, null, null, null],
+         [{ type: 'title', title: '', columnAlignment: 'left' }, null, null, null, null]
+      ];
+      assert.deepEqual(obj.getTableClone(), table);
+   });
+
+
+   it('throws a range error for negative row index argument', () => {
+      expect(() => new InternalTableModel(2, 2).addRowAt(-1)).to.throw(RangeError, 'Index must not be negative');
+   });
+
+
+   it('throws a range error for trying to add new first line (reserved for title elements)', () => {
+      expect(() => new InternalTableModel(2, 2).addRowAt(0)).to.throw(RangeError, 'Can not add new first row due to it being reserved for title objects');
+   });
 
 });
