3635dd1d547112a8377d27b42ff873ca608d0f16
Lucas Vossen <lv_code@outlook.com>
2023-06-17 23:35:31 +0200

    add function parseTitleSeparator()

--- src/parser/parseMarkdownToInternalTable.ts
+++ src/parser/parseMarkdownToInternalTable.ts
@@ -1,3 +1,6 @@
+import { TitleContent, ColumnAlignmentOption } from '../model/types';
+
+
 function parseMarkdownTableIntoArray(markdownTable: string): string[][] {
    let rowArray = markdownTable.split('\n');
 
@@ -15,4 +18,24 @@ function parseMarkdownTableIntoArray(markdownTable: string): string[][] {
 }
 
 
-export { parseMarkdownTableIntoArray };
+function parseTitleSeparator(separator: string): ColumnAlignmentOption {
+   const isLeftAligned = separator.startsWith(':');
+   const isRightAligned = separator.endsWith(':');
+
+   if (isLeftAligned && isRightAligned) {
+      return 'center';
+   }
+
+   if (isLeftAligned) {
+      return 'left';
+   }
+
+   if (isRightAligned) {
+      return 'right';
+   }
+
+   return 'left';
+}
+
+
+export { parseMarkdownTableIntoArray, parseTitleSeparator };
