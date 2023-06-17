e21cd379ed126e928a8f33acf0772fc73b06c134
Lucas Vossen <lv_code@outlook.com>
2023-06-18 01:41:40 +0200

    add test for function isImageString()

--- test/parser/parseMarkdownToInternalTable.test.ts
+++ test/parser/parseMarkdownToInternalTable.test.ts
@@ -1,7 +1,8 @@
 import { assert } from 'chai';
 import {
    parseMarkdownTableIntoArray,
-   parseTitleSeparator
+   parseTitleSeparator,
+   isImageString
 } from '../../src/parser/parseMarkdownToInternalTable';
 
 
@@ -40,6 +41,36 @@ describe('parseMarkdownToInternalTable.parseTitleSeparator()', () => {
 
    it('parses invalid title separators to result "left"', () => {
       assert.equal(parseTitleSeparator('invalid'), 'left');
+   });
+
+});
+
+
+
+describe('parseMarkdownToInternalTable.isImageString()', () => {
+
+   it('detects valid image string', () => {
+      assert.equal(isImageString('<img src="SRC" alt="ALT" >'), true);
+      assert.equal(isImageString('<img src="SRC">'), true);
+      assert.equal(isImageString('<img src="SRC" alt="ALT">'), true);
+      assert.equal(isImageString('<img src="SRC" alt="ALT" />'), true);
+      assert.equal(isImageString('<img src="SRC" alt="ALT"/>'), true);
+   });
+
+   it('detects invalid image string', () => {
+      assert.equal(isImageString('<im src="SRC" alt="ALT" >'), false);
+      assert.equal(isImageString('img src="SRC" alt="ALT">'), false);
+      assert.equal(isImageString('<im src="SRC" alt="ALT" />'), false);
+      assert.equal(isImageString('<img src="SRC" alt="ALT"'), false);
+      assert.equal(isImageString('<img alt="ALT >"'), false);
+      assert.equal(isImageString('<img alt="ALT>"'), false);
+      assert.equal(isImageString('<img alt="ALT />"'), false);
+      assert.equal(isImageString('<img alt="ALT/>"'), false);
+   });
+
+   it('detects non image string', () => {
+      assert.equal(isImageString(''), false);
+      assert.equal(isImageString('FooBar'), false);
    });
 
 });
