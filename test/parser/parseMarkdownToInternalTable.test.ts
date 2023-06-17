5a8b7ac1b0e5ebb89f72e278f6787da9959f5199
Lucas Vossen <lv_code@outlook.com>
2023-06-17 23:35:55 +0200

    add test for function parseTitleSeparator()

--- test/parser/parseMarkdownToInternalTable.test.ts
+++ test/parser/parseMarkdownToInternalTable.test.ts
@@ -1,5 +1,9 @@
 import { assert } from 'chai';
-import { parseMarkdownTableIntoArray } from '../../src/parser/parseMarkdownToInternalTable';
+import {
+   parseMarkdownTableIntoArray,
+   parseTitleSeparator
+} from '../../src/parser/parseMarkdownToInternalTable';
+
 
 
 const markdownTableDefinition = `|Header1   |Header2 |
@@ -19,6 +23,23 @@ describe('parseMarkdownToInternalTable.parseMarkdownTableIntoArray()', () => {
          ['', '<a href="https://test.com/" target="_blank"><img src="https://test.com/image.jpg" alt="description" width="40" height="40"/> </a>']
       ];
       assert.deepEqual(parseMarkdownTableIntoArray(markdownTableDefinition), expectedResult);
+   });
+
+});
+
+
+
+describe('parseMarkdownToInternalTable.parseTitleSeparator()', () => {
+
+   it('parses valid title separators', () => {
+      assert.equal(parseTitleSeparator(':--------'), 'left');
+      assert.equal(parseTitleSeparator('--------:'), 'right');
+      assert.equal(parseTitleSeparator(':-------:'), 'center');
+   });
+
+
+   it('parses invalid title separators to result "left"', () => {
+      assert.equal(parseTitleSeparator('invalid'), 'left');
    });
 
 });
