0041bc28347315beaaa052434c308e8ca6513702
Lucas Vossen <lv_code@outlook.com>
2023-06-25 01:45:46 +0200

    add row highlighting

--- src/view/htmlTable/tableStructureComponents/TableEditorAddRowControl/TableEditorAddRowControl.tsx
+++ src/view/htmlTable/tableStructureComponents/TableEditorAddRowControl/TableEditorAddRowControl.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { ReactElement } from 'react';
+import { TablePosition } from '../../../../model/types';
 import './tableEditorAddRowControl.css';
 
 
 type Props = {
-   columnIndex: number,
+   position: TablePosition,
    selectedColumnIndex: number,
+   highlightedRowIndex: number,
    setSelectedColumnIndex: (a: number) => void,
    addColumnToInternalTable: (index: number) => void
 };
@@ -14,19 +16,38 @@ type Props = {
 
 function TableEditorAddRowControl(props: Props): ReactElement {
    const {
-      columnIndex,
+      position,
       selectedColumnIndex,
+      highlightedRowIndex,
       setSelectedColumnIndex,
       addColumnToInternalTable
    } = props;
 
 
+   function isControlActive(): boolean {
+      return selectedColumnIndex === position.columnIndex;
+   }
+
+
+   function isRowHighlightActive(): boolean {
+      return highlightedRowIndex === position.rowIndex;
+   }
+
+
    function generateStyleClass(): string {
-      const isControlActive = selectedColumnIndex === columnIndex;
-      if (isControlActive) {
-         return 'table-editor-add-row-control-active';
+      const styleList: string[] = [];
+
+      if (isControlActive()) {
+         styleList.push('table-editor-add-row-control-active');
+      } else {
+         styleList.push('table-editor-add-row-control');
       }
-      return 'table-editor-add-row-control';
+
+      if (isRowHighlightActive()) {
+         styleList.push('table-editor-add-row-control-row-hightlight-active');
+      }
+
+      return styleList.join(' ');
    }
 
 
@@ -34,9 +55,9 @@ function TableEditorAddRowControl(props: Props): ReactElement {
       <div className="table-editor-add-row-control-wrapper">
          <div
             className={generateStyleClass()}
-            onPointerEnter={() => setSelectedColumnIndex(columnIndex)}
+            onPointerEnter={() => setSelectedColumnIndex(position.columnIndex)}
             onPointerLeave={() => setSelectedColumnIndex(-1)}
-            onClick={() => addColumnToInternalTable(columnIndex)}
+            onClick={() => addColumnToInternalTable(position.columnIndex)}
          />
       </div>
    );
