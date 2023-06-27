5aa468ffa4dc48799533b86ba404042e2f459719
Lucas Vossen <lv_code@outlook.com>
2023-06-27 21:36:11 +0200

    add component

---
+++ src/view/pages/landingPage/LandingPage.tsx
@@ -0,0 +1,38 @@
+import React, { ReactElement } from 'react';
+import { PageId } from '../pageIdType';
+import './landingPage.css';
+
+
+type Props = {
+   setPageId: (id: PageId) => void
+};
+
+
+function LandingPage(props: Props): ReactElement {
+   const { setPageId } = props;
+
+   return (
+      <div className="landing-page-wrapper">
+         <div className="landing-page-control-wrapper">
+            <h2>Markdown Table Generator</h2>
+            <button
+               type="button"
+               className="landing-page-button"
+               onClick={() => setPageId('table-dimension-input-page')}
+            >
+               Create New Table
+            </button>
+            <button
+               type="button"
+               className="landing-page-button"
+               onClick={() => setPageId('markdown-input-page')}
+            >
+               Edit Existing Table
+            </button>
+         </div>
+      </div>
+   );
+}
+
+
+export { LandingPage };
