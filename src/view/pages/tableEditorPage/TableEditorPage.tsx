b6d0f206cc6ac3978a6237f4e94764f315be5dde
Lucas Vossen <lv_code@outlook.com>
2023-06-28 00:52:20 +0200

    add component

---
+++ src/view/pages/tableEditorPage/TableEditorPage.tsx
@@ -0,0 +1,34 @@
+import React, { ReactElement } from 'react';
+import { InternalTableModel } from '../../../model/InternalTableModel';
+import { TableEditor } from '../../subcomponents/tableEditor/TableEditor';
+import { PageId } from '../pageIdType';
+import './tableEditorPage.css';
+
+
+type Props = {
+   internalTable: InternalTableModel,
+   setPageId: (id: PageId) => void
+};
+
+
+function TableEditorPage(props: Props): ReactElement {
+   const { internalTable, setPageId } = props;
+
+   return (
+      <div className="table-editor-page-wrapper">
+         <h2>Table Editor</h2>
+         <TableEditor internalTable={internalTable} />
+         <div className="table-editor-page-control-wrapper">
+            <button type="button" onClick={() => setPageId('landing-page')}>
+               Back
+            </button>
+            <button type="button" onClick={() => setPageId('markdown-output-page')}>
+               Generate Markdown
+            </button>
+         </div>
+      </div>
+   );
+}
+
+
+export { TableEditorPage };
